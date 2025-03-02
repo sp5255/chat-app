4f0728c8534e17dcd4a23c8c71434131351f7c0c
sp5255 <pundir5255@gmail.com>
2025-03-02 21:11:55 +0530

    refactor: added validation for participants

--- app-backend/models/conversation.model.js
+++ app-backend/models/conversation.model.js
@@ -8,8 +8,18 @@ const conversationSchema = new Schema({
         ref: "User",
       },
     ],
-    validate: (arr) => {
-      return arr.length > 1;
+    validate: {
+      validator: (participants) => {
+        participants = participants.map((userId) => userId.toString());
+        if (new Set(participants).size !== participants.length) return false;
+        return participants.length > 1;
+      },
+      message: (props) => {
+        props.value = props.value.map((userId) => userId.toString());
+        if (new Set(props.value).size !== props.value.length)
+          return "participants should be unique";
+        return "there should be atlease 1 participant for conversation";
+      },
     },
   },
   name: {
@@ -21,6 +31,7 @@ const conversationSchema = new Schema({
     ref: "Message",
   },
   createAt: { type: Date, default: Date.now },
+  isDeleted: { type: Boolean, default: false },
 });
 
 const Conversation = model("Conversation", conversationSchema);
