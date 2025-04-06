cf98c4882a770c6f0e38cadb31efd992f871fdcd
sp5255 <pundir5255@gmail.com>
2025-04-06 22:00:02 +0530

    refactor[chats page]: replaced fetch to axios

--- app-frontend/src/app/all_chats/page.jsx
+++ app-frontend/src/app/all_chats/page.jsx
@@ -1,23 +1,13 @@
-import { BASE_URL } from "const";
+import axios from "lib/axiosIntstance";
 import Chat from "./Chat";
 import Messages from "./Messages";
-import { cookies } from "next/headers";
 
 const getAllConversations = async () => {
-  const cookieStore = await cookies();
 
-  const resp = await fetch(BASE_URL + "/conversation/all", {
+  const resp = await axios("/conversation/all", {
     method: "GET",
-    headers: {
-      Authorization: `${cookieStore.get("token")?.value}`,
-      "Content-Type": "application/json", // Opti
-    },
   });
-  if (resp.ok) {
-    const chats = await resp.json();
-    return chats;
-  }
-  return;
+  return resp.data;
 };
 
 const AllChats = async () => {
@@ -29,15 +19,16 @@ const AllChats = async () => {
   // });
 
   const chats = await getAllConversations();
-  console.log("chats : ", chats);
 
   if (!Array.isArray(chats)) {
     // render a error popup
+    return <>Error</>;
   }
   return (
     <div className="flex">
       <div className="overflow-y-auto max-h-[100vh] scrollbar-hide max-w-72 w-full">
-        {Array.isArray(chats) && chats?.map((chat) => <Chat chatData={chat} />)}
+        {Array.isArray(chats) &&
+          chats?.map((chat) => <Chat chatData={chat} key={chat?.id} />)}
       </div>
       <div>
         <Messages />
